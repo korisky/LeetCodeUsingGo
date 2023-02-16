fe92500d42520ff1afd25aa3fc3752f71e945b37
roylic <lykismekk@outlook.com>
2023-02-16 23:11:38 +0800

    adding solution by using 1-d array acting like map, and only traverse the string once

--- Ex_3_LongSubNoRepeat/LongSubNoRepeat.go
+++ Ex_3_LongSubNoRepeat/LongSubNoRepeat.go
@@ -11,11 +11,9 @@ func main() {
 }
 
 func lengthOfLongestSubstring(s string) int {
-
 	if len(s) < 2 {
 		return len(s)
 	}
-
 	left, right, max := 0, 0, 0
 	for i := 0; i < len(s); i++ {
 		for right = left; right < i; right++ {
@@ -25,6 +23,29 @@ func lengthOfLongestSubstring(s string) int {
 			}
 		}
 	}
-
 	return int(math.Max(float64(max), float64(len(s)-left)))
+}
+
+func lengthOfLongestSubstring_Map(s string) int {
+	if len(s) < 2 {
+		return len(s)
+	}
+	// use char's ascii number as idx, make 1-d array to store
+	idxMap := make([]int, 256)
+	maxSubLen := 0
+	curSubLen := 1
+	for i := 0; i < len(s); i++ {
+		cha := s[i]
+		lastChaPos := idxMap[cha]
+		idxMap[cha] = i + 1
+		toLastLen := i - lastChaPos + 1
+		if toLastLen < curSubLen {
+			curSubLen = toLastLen
+		}
+		if curSubLen > maxSubLen {
+			maxSubLen = curSubLen
+		}
+		curSubLen++
+	}
+	return maxSubLen
 }
