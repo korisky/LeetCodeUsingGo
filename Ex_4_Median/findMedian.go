68045dc1aff6809ec6928b59add436c4e98206df
roylic <lykismekk@outlook.com>
2023-02-18 14:39:40 +0800

    calculation upgrade

--- Ex_4_Median/findMedian.go
+++ Ex_4_Median/findMedian.go
@@ -42,3 +42,37 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return float64(mSlice[p3-1])
 	}
 }
+
+func findMedianSortedArrays_Improve(nums1 []int, nums2 []int) float64 {
+
+	n, m := len(nums1), len(nums2)
+	mSlice := make([]int, (n+m)/2+1)
+	even := (n+m)&1 == 0
+
+	p1, p2, p3 := 0, 0, 0
+	for p1 < n && p2 < m && p3 < len(mSlice) {
+		if nums1[p1] <= nums2[p2] {
+			mSlice[p3] = nums1[p1]
+			p1++
+		} else {
+			mSlice[p3] = nums2[p2]
+			p2++
+		}
+		p3++
+	}
+	for p1 < n && p3 < len(mSlice) {
+		mSlice[p3] = nums1[p1]
+		p1++
+		p3++
+	}
+	for p2 < m && p3 < len(mSlice) {
+		mSlice[p3] = nums2[p2]
+		p2++
+		p3++
+	}
+
+	if even && n+m > 1 {
+		return (float64(mSlice[p3-1]) + float64(mSlice[p3-2])) / 2
+	}
+	return float64(mSlice[p3-1])
+}
