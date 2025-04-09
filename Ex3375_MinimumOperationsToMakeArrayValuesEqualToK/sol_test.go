90509cf303b419959ec7ba385d34c28b51b2b25e
Roylic <35546172+korisky@users.noreply.github.com>
2025-04-09 23:08:14 +0800

    chore: add solution from Leetcode's idea for #3375

---
+++ Ex3375_MinimumOperationsToMakeArrayValuesEqualToK/sol_test.go
@@ -0,0 +1,26 @@
+package Ex3375_MinimumOperationsToMakeArrayValuesEqualToK
+
+import "testing"
+
+func Test_Func(t *testing.T) {
+	arr := make([]int, 0)
+	arr = append(arr, 5)
+	arr = append(arr, 2)
+	arr = append(arr, 5)
+	arr = append(arr, 4)
+	arr = append(arr, 5)
+
+	println(minOperations(arr, 2))
+}
+
+func minOperations(nums []int, k int) int {
+	st := make(map[int]bool)
+	for _, x := range nums {
+		if x < k {
+			return -1
+		} else {
+			st[x] = true
+		}
+	}
+	return len(st)
+}
